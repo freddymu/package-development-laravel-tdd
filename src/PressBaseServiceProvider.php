e43c17b1922909fb8bc57b0b39e91a474c101e53
Freddy <fredhopelane@gmail.com>
2020-05-31 16:28:56 +0700

    add config file and Laravel publishable package assets

--- src/PressBaseServiceProvider.php
+++ src/PressBaseServiceProvider.php
@@ -11,6 +11,10 @@ class PressBaseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->registerPublishing();
+        }
+
         $this->registerResources();
     }
 
@@ -24,5 +28,12 @@ class PressBaseServiceProvider extends ServiceProvider
     private function registerResources()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    protected function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/press.php' => config_path('press.php')
+        ], 'press-config');
     }
 }
