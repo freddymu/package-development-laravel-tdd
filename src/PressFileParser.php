b409cdc65dc5c5a012c1baf5a532d4465bbe56ef
Freddy <fredhopelane@gmail.com>
2020-05-31 13:01:53 +0700

    add new unit testing for head

--- src/PressFileParser.php
+++ src/PressFileParser.php
@@ -8,14 +8,16 @@ use Illuminate\Support\Facades\File;
 
 class PressFileParser
 {
-    private $filename;
-    private $data;
+    protected $filename;
+    protected $data;
 
     public function __construct($filename)
     {
         $this->filename = $filename;
 
         $this->splitFile();
+
+        $this->explodeData();
     }
 
     public function getData()
@@ -23,11 +25,19 @@ class PressFileParser
         return $this->data;
     }
 
-    public function splitFile()
+    protected function splitFile()
     {
         preg_match('/^\-{3}(.*?)\-{3}(.*)/s',
             File::get($this->filename),
             $this->data
         );
+    }
+
+    protected function explodeData()
+    {
+        foreach (explode("\n", trim($this->data[1])) as $fieldString){
+            preg_match('/(.*):\s?(.*)/', $fieldString, $fieldArray);
+            $this->data[$fieldArray[1]] = trim($fieldArray[2]);
+        }
     }
 }
