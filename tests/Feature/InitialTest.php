7dfdb2cc20bec1d3fbb9f2806a68949387919f28
Freddy <fredhopelane@gmail.com>
2020-05-31 12:20:45 +0700

    getting started with PHPUnit, Composer and PSR4 Autoloading

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace freddymu\Press\Tests\Feature;
+
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+
+}
